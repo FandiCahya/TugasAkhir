cdeca3a9838b464a2208491d28144ad31b726188
FandiCahya <fandicahya56@gmail.com>
2025-02-28 11:22:07 +0700

    Backend: Api Update Status Pengajuan

--- app/src/main/java/com/example/applicationsop/models/PengajuanRequest.kt
+++ app/src/main/java/com/example/applicationsop/models/PengajuanRequest.kt
@@ -4,12 +4,17 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PengajuanRequest(
-    val tgl: String,
-    val nama_sistem: String,
-    val jenis: String,
-    val rencana_anggaran: String,
-    val masalah: String,
-    val output: String,
+    val tgl: String?= null,
+    val nama_sistem: String?= null,
+    val jenis: String?= null,
+    val rencana_anggaran: String?= null,
+    val masalah: String?= null,
+    val output: String?= null,
     val status: String? = null,
-    val user_id: String
+    val user_id: String?= null
+)
+
+@Serializable
+data class UpdateStatusPengajuan(
+    val status: String? = null,
 )
